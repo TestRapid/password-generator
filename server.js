34eba369205880c4ee45c0cb8140fb53b48c11b9
test <test@td.com>
2021-08-22 13:47:56 +0530

    third commit

--- server.js
+++ server.js
@@ -2,4 +2,13 @@ const program = require("commander");
 
 program.version("1.0.0").description("Simple Password Generator");
 
-program.option("--length", "length of the password").parse();
+program
+	.option("-l, --length <number>", "length of the password")
+	.option("-s, --save", "save password to password.txt")
+	.option("-nn, --no-numbers", "no numbers")
+	.option("-ns, --no-symbols", "no numbers")
+	.parse();
+
+const options = program.opts();
+
+console.log(options);
