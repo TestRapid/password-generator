66e30ea4e3071a199e3d18df931f3ad99368523d
test <test@td.com>
2021-08-22 17:14:02 +0530

    final commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const program = require("commander");
 const chalk = require("chalk");
 const clipboardy = require("clipboardy");
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+
 const log = console.log;
 
 program.version("1.0.0").description("Simple Password Generator");
@@ -16,6 +20,16 @@ const { length, save, numbers, symbols } = program.opts();
 
 // generating password
 const generatedPassword = createPassword(length, numbers, symbols);
+
+// saving password
+if (save) {
+	savePassword(generatedPassword);
+}
+
+clipboardy.writeSync(generatedPassword);
+
+log(chalk.blue(`Generated Password : `) + chalk.bold(generatedPassword));
+log(chalk.yellow(`Password is copied to clipboard`));
 
 // functions
 function createPassword(length = 8, hasNumbers = true, hasSymbols = true) {
@@ -34,12 +48,12 @@ function createPassword(length = 8, hasNumbers = true, hasSymbols = true) {
 
 	return password;
 }
-
-if (save) {
-	clipboardy.writeSync(generatedPassword);
-}
-
-log(chalk.blue(`Generated Password : `) + chalk.bold(generatedPassword));
-if (save) {
-	log(chalk.yellow(`Password is copied to clipboard`));
+function savePassword(password) {
+	fs.open(path.join(__dirname, "passwords.txt"), "a", 666, (e, id) => {
+		fs.write(id, password + os.EOL, null, "utf-8", () => {
+			fs.close(id, () => {
+				console.log(chalk.green(`Password has saved to passwords.txt`));
+			});
+		});
+	});
 }
